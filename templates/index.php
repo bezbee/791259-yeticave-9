42f62cdaf8f2f9adb37033c7a87be6d4cdf0ce05
xena <zizu087@gmail.com>
2019-04-18 22:16:36 -0600

    Added Xss protection

--- templates/index.php
+++ templates/index.php
@@ -4,7 +4,7 @@
     <ul class="promo__list">
         <?php foreach ($categories as $category ): ?>
             <li class="promo__item promo__item--boards">
-                <a class="promo__link" href="pages/all-lots.html"><?=$category; ?></a>
+                <a class="promo__link" href="pages/all-lots.html"><?=htmlspecialchars($category); ?></a>
             </li>
         <?php endforeach; ?>
     </ul>
@@ -17,15 +17,15 @@
         <?php foreach ($lots as $key => $item): ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="<?=$item['url']; ?>" width="350" height="260" alt="<?=$item['brand']; ?>">
+                    <img src="<?=$item['url']; ?>" width="350" height="260" alt="<?=htmlspecialchars($item['brand']); ?>">
                 </div>
                 <div class="lot__info">
                     <span class="lot__category"></span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= $item['brand']; ?></a></h3>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=htmlspecialchars($item['brand']); ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?=formatPrice($item['price']); ?></span>
+                            <span class="lot__cost"><?=formatPrice(htmlspecialchars($item['price'])); ?></span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
